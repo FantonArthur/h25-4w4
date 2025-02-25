978bb674ad5287b84906d6918d81e8ba6332fec3
FantonArthur <e2359875@cmaisonneuve.qc.ca>
2025-02-25 11:10:56 -0500

    s6c1 on essaye de debugger le wp

--- functions.php
+++ functions.php
@@ -1,27 +1,17 @@
-<?php 
-// fonction php 
-?>
+<?php
 
-
-<?php  
-function ajouter_css_dans_header() {
-
-  wp_enqueue_style(
-    'style_normalize',
-    get_template_directory_uri() . '/normalize.css',
-    array(),
-    filemtime(get_template_directory() . '/normalize.css'));
-
-
-    wp_enqueue_style(
-        'style_principal',
-        get_template_directory_uri() . '/style.css',
-        array(),
-        filemtime(get_template_directory() . '/style.css'));
+function mon_theme_supports() {
+  add_theme_support('title-tag');
+  add_theme_support('menus');
+  add_theme_support('post-thumbnails');
 }
+add_action('after_setup_theme', 'mon_theme_supports');
 
-add_action('wp_enqueue_scripts', 'ajouter_css_dans_header');
-
+function theme_tp_enqueue_styles() { 
+  wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css'); 
+  wp_enqueue_style('main-style', get_stylesheet_uri()); 
+} 
+add_action('wp_enqueue_scripts', 'theme_tp_enqueue_styles');
 
 /**
  * Modifie la requete principale de WordPress avant qu'elle soit exécuté
@@ -32,11 +22,13 @@ add_action('wp_enqueue_scripts', 'ajouter_css_dans_header');
  */
 function modifie_requete_principal( $query ) {
   if ( $query->is_home() && $query->is_main_query() && ! is_admin() ) {
-    $query->set( 'category_name', 'nouvelle' );
+    $query->set( 'category_name', 'populaire' );
     $query->set( 'orderby', 'title' );
     $query->set( 'order', 'ASC' );
-    }
-   }
-   add_action( 'pre_get_posts', 'modifie_requete_principal' );
+
+    Error_log('Requête modifiée');
+  }
+}
+add_action( 'pre_get_posts', 'modifie_requete_principal' );
 
 ?>
