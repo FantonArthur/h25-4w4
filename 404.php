31e39476e6f318e0041a86860d411425eaef6c83
FantonArthur <e2359875@cmaisonneuve.qc.ca>
2025-03-25 09:33:54 -0400

    s9c1 page 404.php

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<section class="error-404 not-found">
+    <header class="page-header">
+        <h1 class="page-title">Page non trouvée</h1>
+    </header><!-- .page-header -->
+
+    <div class="page-content">
+        <p>Désolé, mais rien ne correspond à votre recherche. Essayez de nouveau avec des mots-clés différents.</p>
+        <?php get_search_form(); ?>
+    </div><!-- .page-content -->
+</section><!-- .error-404 -->
+
+<?php get_footer(); ?>
