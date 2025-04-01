82311969e814ae71ef68c9f33111b3171fd9917f
FantonArthur <e2359875@cmaisonneuve.qc.ca>
2025-04-01 10:03:37 -0400

    ajout de l'image background et debut du style

--- 404.php
+++ 404.php
@@ -2,12 +2,13 @@
 
 <?php $erreur_titre = get_theme_mod('erreur_titre', 'Default title'); ?>
 <?php $erreur_explication = get_theme_mod('erreur_explication', ''); ?>
+<?php $erreur_bg = get_theme_mod('erreur_image', ''); ?>
 
-<section class="error-404 not-found">
+<section class="error-404 not-found" style="background-image: url(<?php echo esc_url($erreur_bg); ?>); width: 100%; height: 100vh; background-size: cover; background-position: center;">
     <header class="page-header">
         <h1 class="page-title"><?= $erreur_titre; ?></h1>
     </header>
-    <div class="page-content">
+    <div class="page-content" >
         <p><?php echo $erreur_explication; ?></p>
         <!-- Bouton retour a l'accueil -->
         <?php wp_nav_menu(array(
